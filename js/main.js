bb0bd35e44460185af942ea36e9007c835dd3152
samandar-dev <samandarbohodirov01@gmail.com>
2022-01-26 12:43:39 +0500

    creatt

---
+++ js/main.js
@@ -0,0 +1,20 @@
+document.getElementById('modeBtn').addEventListener('click', function() {
+  // document.getElementById('lig-dark').classList.toggle('dark');
+  document.body.classList.toggle('dark');
+})
+
+
+
+
+// let a = 0;
+// document.getElementById('modeBtn').addEventListener('click', function () {
+//   // document.getElementById('header').classList.toggle('dark');
+//   document.body.classList.toggle('dark');
+//   if (a == 0) {
+//   //   this.textContent = 'Light mode';
+//   //   a++;
+//   // } else {
+//   //   this.textContent = 'Dark mode';
+//   //   a = 0;
+//   }
+// });
